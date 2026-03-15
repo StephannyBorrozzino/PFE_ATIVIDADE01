19b5f7749600a4efb9eb4b0383a5b3ccfb5f5826
StephannyBorrozzino <154994245+StephannyBorrozzino@users.noreply.github.com>
2026-03-15 10:46:20 -0300

    adição de produtos, criação das categorias e melhorias gerais

--- js/script.js
+++ js/script.js
@@ -3,29 +3,63 @@ const contadorCarrinho = document.querySelector(".contadorCart");
 
 var quantCarrinho = 0;
 var totalCarrinho = 0;
+var listaNomeCarrinho = [];
+var listaPrecoCarrinho = [];
+var mensagem = "";
 
 botoes.forEach(botao => {
     botao.addEventListener("click", () => {
 
-      const nome = botao.dataset.nome;
-      const preco = Number(botao.dataset.preco);
+        const nome = botao.dataset.nome;
+        const preco = Number(botao.dataset.preco);
 
-    quantCarrinho++;
-    totalCarrinho += preco;
+        listaNomeCarrinho.push(nome);
+        listaPrecoCarrinho.push(preco);
 
-    contadorCarrinho.textContent = quantCarrinho;
+        quantCarrinho++;
+        totalCarrinho += preco;
 
-    alert("Produto Adicionado com Sucesso! 👍")
-  });
+        contadorCarrinho.textContent = quantCarrinho;
+
+        alert(`Produto Adicionado com Sucesso! 👍 \n ${nome} - R$ ${preco.toFixed(2)}`)
+    });
 });
 
-function finalizarCompra(){
-    if(quantCarrinho == 0){
+function finalizarCompra() {
+    if (quantCarrinho == 0) {
         alert(`Coloque itens no carrinho primeiro antes de finalizar!`);
-    }else {
-        alert(`Compra Finalizada com Sucesso! \n Obrigada por compra na Loja Midnight Geek ⭐ \n Total no carrinho: R$ ${totalCarrinho.toFixed(2)}`);
+    } else {
+        mensagem = "";
+
+        for (let i = 0; i < listaNomeCarrinho.length; i++) {
+            mensagem += listaNomeCarrinho[i] + " -  R$ " + listaPrecoCarrinho[i].toFixed(2) + "\n ";
+        }
+
+        alert(`Compra Finalizada com Sucesso! \n Obrigada por comprar na Loja Midnight Geek ⭐ \n Total no carrinho: R$ ${totalCarrinho.toFixed(2)} \n \n Produtos: \n ${mensagem}`);
         quantCarrinho = 0;
         totalCarrinho = 0;
+        listaNomeCarrinho = [];
+        listaPrecoCarrinho = [];
+        mensagem = "";
         contadorCarrinho.textContent = quantCarrinho;
     }
 }
+
+// botão voltar ao topo
+
+const botao = document.getElementById("botaoTopo");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        botao.classList.add("mostrar");
+    } else {
+        botao.classList.remove("mostrar");
+    }
+});
+
+botao.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
